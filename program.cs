3ad9d06af7964cb78786bb336fd426cacccd351a
stuNero <max.vemic@student.nbi-handelsakademin.se>
2025-12-09 15:31:29 +0100

    add DBQueries class
    To organize and hold all queries, CRUD and selects

--- program.cs
+++ program.cs
@@ -1,6 +1,16 @@
+global using MySql.Data.MySqlClient;
+using server;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapDelete("/db",db_reset_to_default);
+
+
+
+async Task db_reset_to_default()
+{
+    
+}
 
 app.Run();
