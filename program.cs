97ed2b6f4ce07b700051d7d89f5fb6056453c1db
amir247 <amir_hamza@outlook.com>
2025-12-10 10:11:17 +0100

    add config, method to reset database
    to be able to reset the database

--- program.cs
+++ program.cs
@@ -2,15 +2,19 @@ global using MySql.Data.MySqlClient;
 using server;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Config config = new("server=127.0.0.1;uid=Holidaymaker;pwd=Holidaymaker;database=Holidaymaker;");
+builder.Services.AddSingleton(config);
 var app = builder.Build();
 
-app.MapDelete("/db",db_reset_to_default);
+app.MapDelete("/db", db_reset_to_default);
 
 
+app.Run();
 
 async Task db_reset_to_default()
 {
-    
+  await MySqlHelper.ExecuteNonQueryAsync(config.db, DBQueries.DropAllTable());
+  await MySqlHelper.ExecuteNonQueryAsync(config.db, DBQueries.CreateUsersTable());
+  await MySqlHelper.ExecuteNonQueryAsync(config.db, DBQueries.CreateCuisinesTable());
 }
-
-app.Run();
