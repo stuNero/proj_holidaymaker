cb5087d64282365e3fd2d67cec94ba3ee72366fc
amir247 <amir_hamza@outlook.com>
2025-12-11 11:37:43 +0100

    changed the dot net version to 8
    for compatibility sake

--- program.cs
+++ program.cs
@@ -21,3 +21,5 @@ async Task db_reset_to_default()
   await MySqlHelper.ExecuteNonQueryAsync(config.db, DBQueries.DropAllTable());
   await MySqlHelper.ExecuteNonQueryAsync(config.db, DBQueries.CreateAllTables());
 }
+
+
