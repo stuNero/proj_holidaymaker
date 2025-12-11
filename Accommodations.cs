6b8abee74170bdf1c997ffd2b67ec91f0fede35a
stuNero <max.vemic@student.nbi-handelsakademin.se>
2025-12-11 15:39:02 +0100

    add Accommodations class, Get, Get_All
    includes WIP code on Delete and Post

---
+++ Accommodations.cs
@@ -0,0 +1,80 @@
+using Microsoft.VisualBasic;
+using Mysqlx.Crud;
+
+namespace server;
+
+class Accommodations
+{
+    // public record Delete_Data(int id);
+    // public static async Task<Delete_Data?> Delete(int id,Config config)
+    // {
+    //     string query = 
+    //     """
+    //     DELETE FROM accommodations
+    //     WHERE id = @id
+    //     """;
+    //     var parameters = new MySqlParameter [] {new ("@id",id)};
+    //     await MySqlHelper.ExecuteNonQuery(config.db,query,parameters);
+    // }
+    public record Get_AllData(int id, string name, string city, string type);
+    public static async Task<List<Get_AllData?>> GetAll(Config config)
+    {
+        List<Get_AllData?> result = new();
+        string query = 
+        """
+        SELECT a.id, a.name, c.name, a.type 
+        FROM accommodations a
+        JOIN cities c ON a.city = c.id;
+        """;
+
+        using (var reader = await MySqlHelper.ExecuteReaderAsync(config.db, query))
+        {
+            while (reader.Read())
+            {
+                result.Add(new(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3)));
+            }
+        }
+        return result;
+    }
+
+    public record Get_Data(int id, string name, string city, string type);
+    public static async Task<Get_Data?> Get(int id, Config config)
+    {
+        Get_Data? result = null;
+        string query =
+        """
+        SELECT a.id, a.name, c.name, a.type 
+        FROM accommodations a
+        JOIN cities c ON a.city = c.id
+        WHERE a.id = @id;
+        """;
+        var parameters = new MySqlParameter[] { new ("@id", id)};
+        using (var reader = await MySqlHelper.ExecuteReaderAsync(config.db, query, parameters))
+        {
+            if(reader.Read())
+            {
+                result = new(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3));
+            }
+        }
+        return result;
+    }
+
+    // public record Post_Args(string name, int city, string type);
+    // public static async Task Post(Post_Args Accommodations, Config config)
+    // {
+    //     string query = """
+    //     INSERT INTO accommodations (name, city, type)
+    //     VALUES(@name, @city, @type)
+    //     """;
+    //     var parameters = new MySqlParameter[]
+    //     {
+    //         new("@name", Accommodations.name),
+    //         new("@city", Accommodations.city),
+    //         new("@type", Accommodations.type)
+
+    //     };
+        
+    //     await MySqlHelper.ExecuteNonQuery(config.db, query, parameters);
+    // }
+    
+}
--- program.cs
+++ program.cs
@@ -2,20 +2,24 @@ global using MySql.Data.MySqlClient;
 using server;
 
 var builder = WebApplication.CreateBuilder(args);
-
 Config config = new("server=127.0.0.1;uid=Holidaymaker;pwd=Holidaymaker;database=Holidaymaker;");
 builder.Services.AddSingleton(config);
 var app = builder.Build();
 
+// User Functions
 app.MapPost("/users", Users.Post);
 app.MapGet("/users", Users.GetAll);
 
+// Accommodations Functions
+app.MapGet("/accommodations",Accommodations.GetAll);
+app.MapGet("/accommodations/{id}",Accommodations.Get);
+// app.MapPost("/accommodations",Accommodations.Post);
+// app.MapPut("/accommodations",Accommodations.Put);
+// app.MapPatch("/accommodations",Accommodations.Patch);
+// app.MapDelete("/accommodations",Accommodations.Delete);
 
+// DB functions
 app.MapDelete("/db", db_reset_to_default);
-
-
-app.Run();
-
 async Task db_reset_to_default()
 {
   await MySqlHelper.ExecuteNonQueryAsync(config.db, DBQueries.DropAllTable());
@@ -23,4 +27,4 @@ async Task db_reset_to_default()
   await MySqlHelper.ExecuteNonQueryAsync(config.db, DBQueries.InsertMockData2());
 }
 
-
+app.Run();
