1dbd21951382238d0ab7f0627fd98bb911bdb3ba
amir247 <amir_hamza@outlook.com>
2025-12-16 11:02:23 +0100

    commiting after merging main to my branch

--- DBQueries.cs
+++ DBQueries.cs
@@ -8,23 +8,23 @@ static class DBQueries
     {
         string query =
         """
-        DROP TABLE IF EXISTS orders;
-        DROP TABLE IF EXISTS users;
-        DROP TABLE IF EXISTS cuisines;
-        DROP TABLE IF EXISTS countries;
-        DROP TABLE IF EXISTS cities;
-        DROP TABLE IF EXISTS accommodations;
-        DROP TABLE IF EXISTS packages;
-        DROP TABLE IF EXISTS accommodation_per_package;
-        DROP TABLE IF EXISTS transport_types;
-        DROP TABLE IF EXISTS transports;
-        DROP TABLE IF EXISTS transport_per_order;
-        DROP TABLE IF EXISTS rooms;
         DROP TABLE IF EXISTS booked_rooms;
-        DROP TABLE IF EXISTS room_properties;
         DROP TABLE IF EXISTS properties_per_room;
-        DROP TABLE IF EXISTS amenities;
+        DROP TABLE IF EXISTS rooms;
+        DROP TABLE IF EXISTS accommodation_per_package;
         DROP TABLE IF EXISTS amenities_per_accommodation;
+        DROP TABLE IF EXISTS accommodations;
+        DROP TABLE IF EXISTS transport_per_order;
+        DROP TABLE IF EXISTS transports;
+        DROP TABLE IF EXISTS transport_types;
+        DROP TABLE IF EXISTS cities;
+        DROP TABLE IF EXISTS countries;
+        DROP TABLE IF EXISTS cuisines;
+        DROP TABLE IF EXISTS orders;
+        DROP TABLE IF EXISTS packages;
+        DROP TABLE IF EXISTS users;
+        DROP TABLE IF EXISTS room_properties;
+        DROP TABLE IF EXISTS amenities;
         """;
         return query;
     }
@@ -203,7 +203,6 @@ static class DBQueries
 
             CREATE TABLE IF NOT EXISTS accommodation_per_package
             (
-                id            INT PRIMARY KEY AUTO_INCREMENT,
                 accommodation INT NOT NULL,
                 package       INT NOT NULL,
                 FOREIGN KEY (accommodation) REFERENCES accommodations(id) ON DELETE CASCADE ON UPDATE CASCADE,
