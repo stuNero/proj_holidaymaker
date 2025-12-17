c9b31f7b143e3d8ce779af9910fce3696fe18058
stuNero <max.vemic@student.nbi-handelsakademin.se>
2025-12-17 11:47:29 +0100

    fix overlapping date, bookroom fixed
    entire method now works and handles all known edge cases

--- Booking.cs
+++ Booking.cs
@@ -1,3 +1,4 @@
+using Mysqlx.Crud;
 using MySqlX.XDevAPI.Common;
 
 namespace server;
@@ -5,8 +6,8 @@ namespace server;
 
 static class Booking
 {
-    public record Available_Rooms(int id, string name, int sleepSpots, decimal price);
-    public static async Task<List<Available_Rooms>> CheckAvailability(int id, DateOnly checkIn, DateOnly checkOut, Config config)
+    public record Available_Rooms(int roomId, string name, int sleepSpots, decimal price);
+    public static async Task<List<Available_Rooms>> CheckAvailability(int accommodationId, DateOnly checkIn, DateOnly checkOut, Config config)
     {
         List<Available_Rooms> results = new();
 
@@ -17,11 +18,13 @@ static class Booking
         WHERE accommodation = @id AND id NOT IN(
         SELECT id FROM booked_rooms
         WHERE (@checkIn BETWEEN check_in AND check_out)
-        OR (@checkOut BETWEEN check_in AND check_out));
+        OR (@checkOut BETWEEN check_in AND check_out)
+        OR (check_in BETWEEN @checkIn AND @checkOut)
+        OR (check_out BETWEEN @checkIn AND @checkOut));
         """;
         var parameter = new MySqlParameter[] 
         {
-            new ("@id", id),
+            new ("@id", accommodationId),
             new ("@checkIn", checkIn),
             new ("@checkOut", checkOut)
         };
@@ -36,8 +39,48 @@ static class Booking
         return results;
     }
     public record Success(bool success, string txt);
-    public static async Task<bool> BookRoom(int roomId, DateOnly checkIn, DateOnly checkOut, Config config)
+    public static async Task<bool> BookRoom(int accommodationId, int roomId, DateOnly checkIn, DateOnly checkOut, Config config)
     {
+        // återanvänd kod från checkavailability....
+        List<Available_Rooms> availableRooms = new();
+
+        string findAvailabilityQuery =
+        """
+        SELECT id, name, sleep_spots, price 
+        FROM rooms  
+        WHERE accommodation = @id AND id NOT IN(
+        SELECT id FROM booked_rooms
+        WHERE (@checkIn BETWEEN check_in AND check_out)
+        OR (@checkOut BETWEEN check_in AND check_out)
+        OR (check_in BETWEEN @checkIn AND @checkOut)
+        OR (check_out BETWEEN @checkIn AND @checkOut));
+        """;
+        var availabilityParameter = new MySqlParameter[] 
+        {
+            new ("@id", accommodationId),
+            new ("@checkIn", checkIn),
+            new ("@checkOut", checkOut)
+        };
+
+        using (var reader = await MySqlHelper.ExecuteReaderAsync(config.db, findAvailabilityQuery, availabilityParameter))
+        {
+            while (reader.Read())
+            {
+                availableRooms.Add(new(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2), reader.GetDecimal(3)));
+            }
+        }
+        bool checkRoom = false;
+        foreach (Available_Rooms room in availableRooms)
+        {
+            if (room.roomId == roomId)
+            {
+                checkRoom = true;
+            }
+        }
+        if (!checkRoom)
+        {
+            return false;
+        }
         string query = 
         """
         INSERT INTO bookings_per_rooms (room, check_in, check_out)
