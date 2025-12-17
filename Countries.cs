e2fe3c38bc07bc401039e7f393b7ef54b229db5d
amir247 <amir_hamza@outlook.com>
2025-12-17 11:45:55 +0100

    add getall method for countries

--- Countries.cs
+++ Countries.cs
@@ -17,4 +17,27 @@ static class Countries
     };
     int query_result = await MySqlHelper.ExecuteNonQueryAsync(config.db, query, parameter);
   }
+
+
+
+  // Get All
+  public record GetAll_Data(int Id, string Name, string Cuisine);
+  public static async Task<List<GetAll_Data>> GetAll(Config config)
+  {
+    List<GetAll_Data> result = new();
+    string query = """
+    SELECT c.id, c.name, cuisines.name
+    FROM countries c
+    INNER JOIN cuisines 
+    ON c.cuisine = cuisines.id
+    """;
+    using (var reader = await MySqlHelper.ExecuteReaderAsync(config.db, query))
+    {
+      while (reader.Read())
+      {
+        result.Add(new(reader.GetInt32(0), reader.GetString(1), reader.GetString(2)));
+      }
+    }
+    return result;
+  }
 }
