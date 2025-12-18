52e8c5c9a0813bc571ac66bc5fb869bd12f7aa31
amir247 <amir_hamza@outlook.com>
2025-12-19 00:19:44 +0100

    add admin access prevlige

--- program.cs
+++ program.cs
@@ -13,15 +13,62 @@ builder.Services.AddSession(option =>
 Config config = new("server=127.0.0.1;uid=Holidaymaker;pwd=Holidaymaker;database=Holidaymaker;");
 builder.Services.AddSingleton(config);
 var app = builder.Build();
-
 app.UseSession();
 
-// DB functions
-app.MapDelete("/db", db_reset_to_default);
+
+
+// Admin access logic
+app.Use(async (context, next) =>
+{
+  PathString path = context.Request.Path;
+  string method = context.Request.Method;
+  // paths that need admin protection
+  bool isAdminPath = path.StartsWithSegments("/users") ||
+                     path.StartsWithSegments("/accommodations") ||
+                     path.StartsWithSegments("/cuisines") ||
+                     path.StartsWithSegments("/countries");
+
+  // paths that are public
+  bool isPublicMethod = (method == "GET" && (path == "/accommodations" || path == "/cuisines" || path == "/countries")) ||
+                        (method == "GET" && (path == "/accommodations/" || path == "/cuisines/" || path == "/countries/")) ||
+                        (method == "POST" && (path == "/login" || path == "/login/")) ||
+                        (method == "POST" && (path == "/users" || path == "/users/"));
+
+
+  if (isAdminPath && !isPublicMethod)
+  {
+    if (!context.Session.IsAvailable || context.Session.GetInt32("user_id") is not int userId)
+    {
+      context.Response.StatusCode = 401;
+      await context.Response.WriteAsJsonAsync(new { message = "Login required" });
+      return;
+    }
+    string query = "SELECT role FROM users WHERE id = @id";
+    var parameter = new MySqlParameter[]
+    {
+      new("@id", userId)
+    };
+
+    object role = await MySqlHelper.ExecuteScalarAsync(config.db, query, parameter);
+    if (role is not string s || s != "admin")
+    {
+      context.Response.StatusCode = 401;
+      await context.Response.WriteAsJsonAsync(new { Message = "You are not authorized!" });
+      return;
+    }
+
+  }
+  await next();
+}
+);
+
+
+
 
 // User Functions
-app.MapPost("/users", Users.Post);
 app.MapGet("/users", Users.GetAll);
+app.MapPost("/users", Users.Post);
+
 
 // Accommodations Functions
 app.MapGet("/accommodations", Accommodations.GetAll);
@@ -54,12 +101,10 @@ app.MapDelete("/countries/{id}", Countries.Delete);
 app.MapPut("/countries/{id}", Countries.Put);
 app.MapPatch("/countries/{id}", Countries.Patch);
 
-// Booking functions
-app.MapGet("/bookings/availability", Booking.CheckAvailability);
-app.MapPost("/bookings/bookroom", Booking.BookRoom);
-app.MapPost("/bookings/book", Booking.Book);
-app.MapGet("/bookings/overview", Booking.Overview);
 
+
+// DB functions
+app.MapDelete("/db", db_reset_to_default);
 async Task db_reset_to_default()
 {
   await MySqlHelper.ExecuteNonQueryAsync(config.db, DBQueries.DropAllTable());
