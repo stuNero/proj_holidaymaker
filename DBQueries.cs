5a2d63ac06408a3523438b8f2088028cc447b0f0
Piercor <pierinocr@gmail.com>
2025-12-18 09:19:27 +0100

    add not null on bookings in bookings_per_rooms
    to avoid booking a room without a booking id.

--- DBQueries.cs
+++ DBQueries.cs
@@ -24,6 +24,120 @@ static class DBQueries
         """;
         return query;
     }
+    static public string CreateAllTables()
+    {
+        string createQueries =
+        """
+           CREATE TABLE IF NOT EXISTS users
+            (
+                id          INT PRIMARY KEY AUTO_INCREMENT,
+                first_name  VARCHAR(255) NOT NULL,
+                last_name   VARCHAR(255) NOT NULL,
+                email       VARCHAR(254) NOT NULL UNIQUE,
+                password    VARCHAR(128),
+                role        ENUM('admin','customer') DEFAULT 'customer'
+            );
+
+            CREATE TABLE IF NOT EXISTS cuisines
+            (
+                id      INT PRIMARY KEY AUTO_INCREMENT,
+                name    VARCHAR(255) UNIQUE
+            );
+
+            CREATE TABLE IF NOT EXISTS countries
+            (
+                id      INT PRIMARY KEY AUTO_INCREMENT,
+                name    VARCHAR(255) UNIQUE,
+                cuisine INT NOT NULL,
+                FOREIGN KEY (cuisine) REFERENCES cuisines(id) ON DELETE RESTRICT ON UPDATE CASCADE
+            );
+
+            CREATE TABLE IF NOT EXISTS cities
+            (
+                id      INT PRIMARY KEY AUTO_INCREMENT,
+                name    VARCHAR(255),
+                country INT NOT NULL,
+                FOREIGN KEY (country) REFERENCES countries(id) ON DELETE RESTRICT ON UPDATE CASCADE
+            );
+
+            CREATE TABLE IF NOT EXISTS accommodations
+            (
+                id      INT PRIMARY KEY AUTO_INCREMENT,
+                name    VARCHAR(255),
+                city    INT NOT NULL,
+                type    ENUM('hotel', 'motel', 'hostel') DEFAULT 'hotel',
+                FOREIGN KEY (city) REFERENCES cities(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+                UNIQUE (city, name)
+            );
+
+            CREATE TABLE IF NOT EXISTS rooms
+            (
+                id            INT PRIMARY KEY AUTO_INCREMENT,
+                name          VARCHAR(255) NOT NULL,
+                sleep_spots   INT NOT NULL,
+                accommodation INT NOT NULL,
+                price         DECIMAL(10,2),
+                FOREIGN KEY (accommodation) REFERENCES accommodations(id) ON DELETE CASCADE ON UPDATE CASCADE
+            );
+            CREATE TABLE IF NOT EXISTS bookings
+            (
+                id          INT PRIMARY KEY AUTO_INCREMENT,
+                user        INT NOT NULL,
+                total_price DECIMAL(10,2) DEFAULT(0.00),
+                FOREIGN KEY (user) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+                UNIQUE (id, user)
+            );
+            CREATE TABLE IF NOT EXISTS bookings_per_rooms
+            (
+                id          INT PRIMARY KEY AUTO_INCREMENT,
+                room        INT NOT NULL,
+                booking     INT NOT NULL,
+                check_in    DATE,
+                check_out   DATE,
+                FOREIGN KEY (room) REFERENCES rooms(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+                FOREIGN KEY (booking) REFERENCES bookings(id),
+                UNIQUE (room, booking, check_in, check_out)
+            );
+
+            CREATE TABLE IF NOT EXISTS room_properties
+            (
+                id   INT PRIMARY KEY AUTO_INCREMENT,
+                name VARCHAR(255) UNIQUE
+            );
+
+            CREATE TABLE IF NOT EXISTS properties_per_room
+            (
+                id       INT PRIMARY KEY AUTO_INCREMENT,
+                room     INT NOT NULL,
+                property INT NOT NULL,
+                FOREIGN KEY (room) REFERENCES rooms(id) ON DELETE CASCADE ON UPDATE CASCADE,
+                FOREIGN KEY (property) REFERENCES room_properties(id) ON DELETE CASCADE ON UPDATE CASCADE,
+                UNIQUE (room, property)
+            );
+
+            CREATE TABLE IF NOT EXISTS amenities
+            (
+                id   INT PRIMARY KEY AUTO_INCREMENT,
+                name VARCHAR(255) UNIQUE
+            );
+
+            CREATE TABLE IF NOT EXISTS amenities_per_accommodation
+            (
+                id            INT PRIMARY KEY AUTO_INCREMENT,
+                amenity       INT NOT NULL,
+                accommodation INT NOT NULL,
+                FOREIGN KEY (amenity) REFERENCES amenities(id) ON DELETE CASCADE ON UPDATE CASCADE,
+                FOREIGN KEY (accommodation) REFERENCES accommodations(id) ON DELETE CASCADE ON UPDATE CASCADE,
+                UNIQUE (amenity, accommodation)
+            );
+            
+            CREATE OR REPLACE VIEW booked_rooms AS
+            SELECT r.id, name, sleep_spots, price, accommodation, check_in, check_out
+            FROM bookings_per_rooms x
+            JOIN rooms r ON x.room = r.id;
+       """;
+        return createQueries;
+    }
     static public string InsertMockData()
     {
         string insertQueries =
@@ -65,13 +179,13 @@ static class DBQueries
 
             INSERT IGNORE INTO rooms (name, sleep_spots, accommodation, price)
             VALUES
-            ('Single Room Classic', 1, (select id from accommodations where name = "Best Western Rome"), 120.00),
-            ('Double Room Classic', 2, (select id from accommodations where name = "Best Western Rome"), 160.00),
-            ('Triple Room Classic', 3, (select id from accommodations where name = "Best Western Rome"), 180.00),
-
             ('Single Room Classic', 1, (select id from accommodations where name = "Best Western Barcelona"), 120.00),
             ('Double Room Classic', 2, (select id from accommodations where name = "Best Western Barcelona"), 160.00),
             ('Triple Room Classic', 3, (select id from accommodations where name = "Best Western Barcelona"), 180.00),
+
+            ('Single Room Classic', 1, (select id from accommodations where name = "Best Western Rome"), 120.00),
+            ('Double Room Classic', 2, (select id from accommodations where name = "Best Western Rome"), 160.00),
+            ('Triple Room Classic', 3, (select id from accommodations where name = "Best Western Rome"), 180.00),
 
             ('Single Room Classic', 1, (select id from accommodations where name = "Best Western Athens"), 120.00),
             ('Double Room Classic', 2, (select id from accommodations where name = "Best Western Athens"), 160.00),
@@ -148,121 +262,5 @@ static class DBQueries
             (2, 7);
         """;
         return insertQueries;
-    }
-
-
-    static public string CreateAllTables()
-    {
-        string createQueries =
-        """
-           CREATE TABLE IF NOT EXISTS users
-            (
-                id          INT PRIMARY KEY AUTO_INCREMENT,
-                first_name  VARCHAR(255) NOT NULL,
-                last_name   VARCHAR(255) NOT NULL,
-                email       VARCHAR(254) NOT NULL UNIQUE,
-                password    VARCHAR(128),
-                role        ENUM('admin','customer') DEFAULT 'customer'
-            );
-
-            CREATE TABLE IF NOT EXISTS cuisines
-            (
-                id      INT PRIMARY KEY AUTO_INCREMENT,
-                name    VARCHAR(255) UNIQUE
-            );
-
-            CREATE TABLE IF NOT EXISTS countries
-            (
-                id      INT PRIMARY KEY AUTO_INCREMENT,
-                name    VARCHAR(255) UNIQUE,
-                cuisine INT NOT NULL,
-                FOREIGN KEY (cuisine) REFERENCES cuisines(id) ON DELETE RESTRICT ON UPDATE CASCADE
-            );
-
-            CREATE TABLE IF NOT EXISTS cities
-            (
-                id      INT PRIMARY KEY AUTO_INCREMENT,
-                name    VARCHAR(255),
-                country INT NOT NULL,
-                FOREIGN KEY (country) REFERENCES countries(id) ON DELETE RESTRICT ON UPDATE CASCADE
-            );
-
-            CREATE TABLE IF NOT EXISTS accommodations
-            (
-                id      INT PRIMARY KEY AUTO_INCREMENT,
-                name    VARCHAR(255),
-                city    INT NOT NULL,
-                type    ENUM('hotel', 'motel', 'hostel') DEFAULT 'hotel',
-                FOREIGN KEY (city) REFERENCES cities(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-                UNIQUE (city, name)
-            );
-
-            CREATE TABLE IF NOT EXISTS rooms
-            (
-                id            INT PRIMARY KEY AUTO_INCREMENT,
-                name          VARCHAR(255) NOT NULL,
-                sleep_spots   INT NOT NULL,
-                accommodation INT NOT NULL,
-                price         DECIMAL(10,2),
-                FOREIGN KEY (accommodation) REFERENCES accommodations(id) ON DELETE CASCADE ON UPDATE CASCADE
-            );
-            CREATE TABLE IF NOT EXISTS bookings
-            (
-                id          INT PRIMARY KEY AUTO_INCREMENT,
-                user        INT NOT NULL,
-                total_price DECIMAL(10,2) DEFAULT(0.00),
-                FOREIGN KEY (user) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
-                UNIQUE (id, user)
-            );
-            CREATE TABLE IF NOT EXISTS bookings_per_rooms
-            (
-                id          INT PRIMARY KEY AUTO_INCREMENT,
-                room        INT NOT NULL,
-                booking     INT,
-                check_in    DATE,
-                check_out   DATE,
-                FOREIGN KEY (room) REFERENCES rooms(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-                FOREIGN KEY (booking) REFERENCES bookings(id),
-                UNIQUE (room, booking, check_in, check_out)
-            );
-
-            CREATE TABLE IF NOT EXISTS room_properties
-            (
-                id   INT PRIMARY KEY AUTO_INCREMENT,
-                name VARCHAR(255) UNIQUE
-            );
-
-            CREATE TABLE IF NOT EXISTS properties_per_room
-            (
-                id       INT PRIMARY KEY AUTO_INCREMENT,
-                room     INT NOT NULL,
-                property INT NOT NULL,
-                FOREIGN KEY (room) REFERENCES rooms(id) ON DELETE CASCADE ON UPDATE CASCADE,
-                FOREIGN KEY (property) REFERENCES room_properties(id) ON DELETE CASCADE ON UPDATE CASCADE,
-                UNIQUE (room, property)
-            );
-
-            CREATE TABLE IF NOT EXISTS amenities
-            (
-                id   INT PRIMARY KEY AUTO_INCREMENT,
-                name VARCHAR(255) UNIQUE
-            );
-
-            CREATE TABLE IF NOT EXISTS amenities_per_accommodation
-            (
-                id            INT PRIMARY KEY AUTO_INCREMENT,
-                amenity       INT NOT NULL,
-                accommodation INT NOT NULL,
-                FOREIGN KEY (amenity) REFERENCES amenities(id) ON DELETE CASCADE ON UPDATE CASCADE,
-                FOREIGN KEY (accommodation) REFERENCES accommodations(id) ON DELETE CASCADE ON UPDATE CASCADE,
-                UNIQUE (amenity, accommodation)
-            );
-            
-            CREATE OR REPLACE VIEW booked_rooms AS
-            SELECT r.id, name, sleep_spots, price, accommodation, check_in, check_out
-            FROM bookings_per_rooms x
-            JOIN rooms r ON x.room = r.id;
-       """;
-        return createQueries;
     }
 }
